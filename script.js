f04fe583b18839a9ce04f52a56bbe22708f726fc
thomas <hopsread@gmail.com>
2026-03-12 18:17:34 -0400

    more

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const name_button = document.getElementbyId("name_button")
-const 
+const name_input = document.getElementbyId("name_input")
+const name_result = document.getElementbyId("name_result")
+
+
+name_button.addEventListener("")

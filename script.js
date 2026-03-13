686ee26de32b760c9d278f935cc3a3a687e88d77
thomas <hopsread@gmail.com>
2026-03-12 20:36:49 -0400

    worked on more stuff, added notes, fixed

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const name_button = document.getElementbyId("name_button")
-const name_input = document.getElementbyId("name_input")
-const name_result = document.getElementbyId("name_result")
+const name_button = document.getElementById("name_button")
+const name_input = document.getElementById("name_input")
+const name_result = document.getElementById("name_result") //some bullshit mistake about capitalization
 
-
-name_button.addEventListener("")
+name_button.addEventListener("click", () => { // detecting for click on button
+  let userInput = name_input.value;            
+  name_result.textContent = "hello " + userInput;  //output 
+})

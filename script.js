86eaef774ef0de0371a7517e1d2a64e54cb7e00c
red <hopsread@gmail.com>
2026-03-13 22:01:50 -0400

    developed a whole bunch more

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ const name_button = document.getElementById("name_button")
 const name_input = document.getElementById("name_input")
 const name_result = document.getElementById("name_result") //some bullshit mistake about capitalization
 
+
 name_button.addEventListener("click", () => { // detecting for click on button
-  let userInput = name_input.value;            
-  name_result.textContent = "hello " + userInput;  //output 
+  let userName = name_input.value;            
+  
+  name_result.textContent = "hello " + userName;  //output 
+  
+  setTimeout(() => {
+    console.log("waiting 3 seconds")
+     document.getElementById("name").style.display = "none" //hiding first name page
+     document.getElementById("tasks").style.display = "block" //showing second page with tasks
+     document.getElementById("greeting_header").textContent = userName; //pushing to html (another case of capitalization)
+     // make sure to capitalize the I in userInput...
+  }, 3000);
+  
+  setTimeout(() => {
+    console.log("waiting two seconds, showing header/name")
+  }, 2000);
 })
